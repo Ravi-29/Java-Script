daf761e3f13a516d9173a52faf2049d3110b723d
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-13 09:27:34 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 234556;
+let accountEmail = "ravi44@gmaol.com";
+var accountPassword = "342442";
+
+
+//accountId = 332432;   //  not allowed because const can't be changed
+accountEmail = "rews234@gmail.com";
+accountPassword = "3242213";
+
+console.log(accountPassword);
