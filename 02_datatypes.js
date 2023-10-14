d9babb80f3c0c9f639a840060fefec9030784051
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-14 09:32:28 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";   // treat all JS code as newer version
+
+let Name = "Ravi"   //string
+let age = 23;    // Integer
+let  pass = true;  // boolean
+let state;        // Undefined
+
+
+console.log(typeof age);
+console.log(3+3);
+
+
+//   Primitive Datatypes         :-         String, Number, Boolean, Null, Undifined, Symbol, BigInt
+
+
+// Non Primitive(reference) DataTypes   :-      Array, Object, Functions
---
+++ 03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = "44acd";
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+
+
+
+//*******************************************************    OPERATION  ********************************************************
+
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+
+//console.log(2**3);    for declaring power
+
+let str1 = "Ravi"
+let str2 = " Shankar"
+let str3 = str1 + str2
+console.log(str3);
+
+
