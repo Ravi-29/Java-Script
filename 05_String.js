4bab34dd7683d4c52f23f72875b6a5d9081c0990
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-15 09:27:35 +0530

    Add files via upload

---
+++ 05_String.js
@@ -0,0 +1,18 @@
+const name = "Ravi";
+const age = 23;
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+
+// const gameName = new String('Raviggg');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.length);
+
+
+const namee = "   Ravi   ";
+console.log(namee);
+console.log(namee.trim());
