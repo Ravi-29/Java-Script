ea86941cb83f64ef99a3e8d3173d74eadb78268f
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-16 09:01:45 +0530

    Add files via upload

---
+++ 06_num_maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+
+const balance = new Number(200);
+
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumbers = 121.2324;
+//console.log(otherNumbers.toPrecision(3));
+
+const numm = 1000000;
+// console.log(numm.toLocaleString('en-IN'));
+
+
+
+
+//************************************************** M A T H S ******************************************************************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(6));
