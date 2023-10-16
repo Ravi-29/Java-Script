1bbba4fee915d9cff8bdd3b444dbc8007b6884ac
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-16 10:04:49 +0530

    Add files via upload

---
+++ 08_arrays.js
@@ -0,0 +1,22 @@
+const arr = [0, 1, 2, 3, 4]
+
+const arr2 = ["Shaktiman","GendaSwami"]
+const arr3 = new Array(1,2,3,4)
+
+console.log(arr[2]);
+
+
+//  Array Methods
+
+// arr.push(5,6,7);
+// arr.pop()
+
+//arr.unshift(9)  // add value at first index
+//arr.shift() // remove the first index value
+
+// console.log(arr.includes(7));  // to find value present in array or not
+// console.log(arr.indexOf(0));
+
+const newArr = arr.join()   // Change the value in string
+console.log(arr);
+console.log(typeof newArr);
