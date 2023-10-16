ba5a1f76b03a162f8dc98ab5f604dad845c16029
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-16 09:02:00 +0530

    Add files via upload

---
+++ 07_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+//let myDates = new Date(2023, 0, 24)
+let myDates = new Date(2023, 0, 24 ,5, 3)
+console.log(myDates.toDateString())
+console.log(myDates.toLocaleString());
