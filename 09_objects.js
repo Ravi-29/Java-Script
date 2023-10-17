d57f49759d58fbb4bbd4d88f8da84672f6254bc5
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-10-17 08:23:55 +0530

    Add files via upload

---
+++ 09_objects.js
@@ -0,0 +1,12 @@
+//    Object Literals
+
+const user = {
+    name: "Ravi",
+    age: 23,
+    rollno: 27,
+    email: "ravi@gbu.ac.in"
+}
+
+console.log(user.email);
+console.log(user["email"]);
+console.log(user)
