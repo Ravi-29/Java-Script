f37eef721bc9cb27bb7e60c9322bc59c0a1f9eba
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-11-26 11:02:08 +0530

    Add files via upload

---
+++ 14_controlflow.js
@@ -0,0 +1,64 @@
+ // if    elseif,    else
+// const temprature = 42
+
+// if (temprature > 50) {
+//     console.log("Less than 50");
+// }else {
+//     console.log("Greater Than 50");
+// }
+
+
+
+
+// const balance = 1000
+
+// // if(balance > 500)  console.log("Pass");
+
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } else if(balance < 750) {
+//     console.log("Less than 750");
+// } else {
+//     console.log("Heee");
+// }
+
+
+
+
+
+// ********************************************SWITCH CASE ***********************************************************
+
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month= 1
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
+
+
+
+
+
