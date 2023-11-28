bec80d3bbf224c56e523df2d47c5f215ad2d1ae4
Ravi Shankar <84237488+Ravi-29@users.noreply.github.com>
2023-11-28 15:49:10 +0530

    Add files via upload

---
+++ 18_mapping.js
@@ -0,0 +1,37 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+// const newNums = myNumbers.map( (num) => num + 10)
+// console.log(newNums);
+
+
+const newNum4 = myNumbers.map( (num) => {
+    return num + 10
+})
+console.log(newNum4);
+
+
+
+// ***************************************************** chaining ********************************************
+
+const newNums = myNumbers
+        .map((num) => num * 10)
+        
+        
+
+console.log(newNums);
+
+const newNums1 = myNumbers
+        .map((num) => num * 10)
+        .map((num) => num + 1)
+        
+
+console.log(newNums1);
+
+
+const newNum = myNumbers
+        .map((num) => num * 10)
+        .map((num) => num + 1)
+        .filter((num) => num >= 40)
+
+console.log(newNum);
+
