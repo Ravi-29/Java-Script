a7e7069c355cf2b895ec3f46381998bd316a6e8a
Ravi Shankar <ravishankar.has@gmail.com>
2024-04-19 18:37:30 +0530

    Add files via upload

---
+++ funn05.js
@@ -0,0 +1,24 @@
+// function statement
+function a() {
+    console.log("a called");
+}
+a();
+
+// function Expression
+var b = function (){
+    console.log("b is called");
+}
+b();
+
+//diffrence btw function statement and function expression is hoistion we cannot call expression before intializing its value
+
+// Function declaration 
+
+function c(){
+    console.log("Hiiiii");
+}
+// Anonymous Function  ::-  Function without name is anonymous function
+// function (){
+
+// }
+
