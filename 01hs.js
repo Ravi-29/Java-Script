f20fb6444b11ad5c8daacf18cc20782aca65388a
Ravi Shankar <ravishankar.has@gmail.com>
2024-06-08 17:35:32 +0530

    Add files via upload

---
+++ 01hs.js
@@ -0,0 +1,9 @@
+const firstName = "Ravi"
+const age = 23;
+const isMarried = "true"
+
+if (isMarried == true) {
+    console.log(firstName + " is married");
+}else {
+    console.log(firstName + " is not married");
+}
